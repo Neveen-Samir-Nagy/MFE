0848a82141ae8acbaca31dc75eabb24d964f4f63
Neveen Samir <nwahba@ejada.com>
2026-06-08 16:17:26 +0300

    Initial Commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import MarketingAppComponent from "./components/MarketingAppComponent";
+
+export default () => {
+    return (
+        <>
+            <h1>Container</h1>
+            <MarketingAppComponent />
+        </>
+    );
+};
