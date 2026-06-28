f7d447f2ea7e2ab61c1011f6bd3ba9863d1443ff
Neveen Samir <nwahba@ejada.com>
2026-06-28 11:26:00 +0300

    add header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import MarketingAppComponent from "./components/MarketingAppComponent";
+import { BrowserRouter } from "react-router-dom";
+import Header from './components/Header';
 
 export default () => {
     return (
         <>
-            <h1>Container</h1>
-            <MarketingAppComponent />
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <MarketingAppComponent />
+                </div>
+            </BrowserRouter>
         </>
     );
 };
