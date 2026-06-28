79c4bb4b1e89aeca42379fcca55967d7d0a0c6b1
Neveen Samir <nwahba@ejada.com>
2026-06-28 12:08:00 +0300

    classnames css

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,16 +2,23 @@ import React from "react";
 import MarketingAppComponent from "./components/MarketingAppComponent";
 import { BrowserRouter } from "react-router-dom";
 import Header from './components/Header';
+import { createGenerateClassName, StylesProvider } from "@material-ui/core";
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co',
+})
 
 export default () => {
     return (
         <>
             <BrowserRouter>
+                <StylesProvider generateClassName={generateClassName}>
                 <div>
                     <Header />
                     <MarketingAppComponent />
                 </div>
-            </BrowserRouter>
+            </StylesProvider>
+        </BrowserRouter >
         </>
     );
 };
